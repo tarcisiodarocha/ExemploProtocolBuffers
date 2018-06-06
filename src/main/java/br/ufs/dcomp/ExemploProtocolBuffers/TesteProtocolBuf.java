9708f3a1b540719d3906fec9d6b68795aa73eeab
Tarcisio da Rocha <tarcisio@dcomp.ufs.br>
2018-06-06 20:36:31 +0000

    primeira versao

--- src/main/java/br/ufs/dcomp/ExemploProtocolBuffers/TesteProtocolBuf.java
+++ src/main/java/br/ufs/dcomp/ExemploProtocolBuffers/TesteProtocolBuf.java
@@ -1,26 +1,29 @@
 package br.ufs.dcomp.ExemploProtocolBuffers;
 
+import java.io.*;
+import com.google.protobuf.util.JsonFormat;
 /**
  * Hello world!
  *
  */
 public class TesteProtocolBuf 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
 
+
         // Agrupando dados do primeiro telefone
-        Aluno.Builder bFone1 = ContatoProto.Telefone.newBuilder();
+        ContatoProto.Telefone.Builder bFone1 = ContatoProto.Telefone.newBuilder();
         bFone1.setNumero("79 99999-0000");
         bFone1.setTipo(ContatoProto.Tipo.MOVEL);
         
         // Agrupando dados do segundo telefone
-        Telefone.Builder bFone2 = ContatoProto.Telefone.newBuilder();
+        ContatoProto.Telefone.Builder bFone2 = ContatoProto.Telefone.newBuilder();
         bFone2.setNumero("79 3200-2323");
         bFone2.setTipo(ContatoProto.Tipo.CASA);
 
         // Agrupando dados do contato do aluno com os dois telefones acima
-        Builder builderAluno = ContatoProto.Aluno.newBuilder();
+        ContatoProto.Aluno.Builder builderAluno = ContatoProto.Aluno.newBuilder();
         builderAluno.setMatricula(123456);
         builderAluno.setNome("Morpheus Santos Sá");
         builderAluno.setEmail("morpheus@dcomp.ufs.br");
@@ -30,22 +33,25 @@ public class TesteProtocolBuf
         // Obtendo o contato do aluno
         ContatoProto.Aluno contatoAluno = builderAluno.build();
         
+        // Serializando o contato 
+        byte[] buffer = contatoAluno.toByteArray();
         
+        // Escrevendo contato já serializado em arquivo
+        FileOutputStream fos = new FileOutputStream(new File("aluno.bin"));
+        fos.write(buffer);
+        fos.close();
         
+        System.out.println("Contato escrito em formato binário no arquivo \"aluno.bin\"");
         
+        // Mapeando a mensagem para o formato json
+        String json = JsonFormat.printer().print(contatoAluno);
         
-        
-/*        ContatoAluno.Aluno mensagemContato = 
-            ContatoProto.Aluno.newBuilder()
-                .setMatricula(123456)
-                .setNome("Morpheus Santos Sá")
-                .setEmail("morpheus@dcomp.ufs.br")
-                .addTelefones(
-                    ContatoProto.Telefone.newBuilder()
-                        .setNumero("79 98888-0000")
-                        .setTipo()
+        // Escrita do conteúdo json em arquivo texto
+        fos = new FileOutputStream(new File("aluno.json"));
+        fos.write(json.getBytes());
+        fos.close();
 
-        .build();
-        */
+        System.out.println("Contato escrito em formato texto/json no arquivo \"aluno.json\"");
+        
     }
 }
